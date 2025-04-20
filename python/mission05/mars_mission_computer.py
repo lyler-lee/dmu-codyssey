d6cc9b7d6f08e24428eab3e6079824c7b7fe8626
이재혁 <127727548+lyler-lee@users.noreply.github.com>
2025-04-20 18:16:22 +0900

    Update mars_mission_computer.py

--- python/mission05/mars_mission_computer.py
+++ python/mission05/mars_mission_computer.py
@@ -1,13 +1,13 @@
-import random   // 랜덤한 난수 생성을 위해 내장 객체 random 모듈 임포트 
-import platform // 파이썬 버전, 윈도우 등의 버전 정보를 가져오기 위해 내장 객체 platform 모듈 임포트
-import os       // 운영체제를 활용하기 위해 내장 객체인 os 모듈 임포트
-import json     // json 형태의 데이터를 처리하기 위해 내장 객체 json 모듈 임포트
+import random   # 랜덤한 난수 생성을 위해 내장 객체 random 모듈 임포트 
+import platform # 파이썬 버전, 윈도우 등의 버전 정보를 가져오기 위해 내장 객체 platform 모듈 임포트
+import os       # 운영체제를 활용하기 위해 내장 객체인 os 모듈 임포트
+import json     # json 형태의 데이터를 처리하기 위해 내장 객체 json 모듈 임포트
 
 
-// 센서의 값을 이용하기 위한 클래스
+# 센서의 값을 이용하기 위한 클래스
 class DummySensor:
     def __init__(self):
-        // 변수를 생성하기 위해 0으로 초기화
+        # 변수를 생성하기 위해 0으로 초기화
         self.env_values = {
         "mars_base_internal_temperature": 0,
         "mars_base_external_temperature": 0,
@@ -17,7 +17,7 @@ class DummySensor:
         "mars_base_internal_oxygen": 0
         }
 
-    // 범위 내 값 랜덤 생성
+    # 범위 내 값 랜덤 생성
     def set_env(self):
         self.env_values["mars_base_internal_temperature"] = random.uniform(18, 30)
         self.env_values["mars_base_external_temperature"] = random.uniform(0, 21)
@@ -26,14 +26,14 @@ class DummySensor:
         self.env_values["mars_base_internal_co2"] = random.uniform(0.02, 0.1)
         self.env_values["mars_base_internal_oxygen"] = random.uniform(4, 7)
 
-    // 값 반환
+    # 값 반환
     def get_env(self):
         return self.env_values
 
-// 미션 컴퓨터와 관련된 클래스
+# 미션 컴퓨터와 관련된 클래스
 class MissionComputer:
     
-    // 미션 컴퓨터의 정보를 가져오기 위한 함수
+    # 미션 컴퓨터의 정보를 가져오기 위한 함수
     def get_mission_computer_info(self):
         try:
             info = {
@@ -46,12 +46,12 @@ class MissionComputer:
             print(json.dumps(info, indent=4))
             return info
             
-        // 컴퓨터의 정보를 가져오는데 문제가 생겼을 경우 예외 출력    
+        # 컴퓨터의 정보를 가져오는데 문제가 생겼을 경우 예외 출력    
         except Exception as e:
             print(f"Error retrieving system info: {e}")
             return {}
 
-    // 미션 컴퓨터의 정보를 알아내기 위한 함수
+    # 미션 컴퓨터의 정보를 알아내기 위한 함수
     def get_mission_computer_load(self):
         try:
             load = {
@@ -61,11 +61,11 @@ class MissionComputer:
             print(json.dumps(load, indent=4))
             return load
             
-        // 컴퓨터의 정보를 가져오는데 문제가 생겼을 경우 예외 출력     
+        # 컴퓨터의 정보를 가져오는데 문제가 생겼을 경우 예외 출력     
         except Exception as e:
             print(f"Error retrieving system load: {e}")
             return {}
-    // 
+    # 
     def _get_memory_size(self):
         try:
             with open('/proc/meminfo', 'r') as mem:
@@ -95,11 +95,15 @@ class MissionComputer:
             return "Unknown"
 
 
+# 파일 실행을 위해 메인 함수 설정
 if __name__ == "__main__":
+    
+    # DummySemsor 클래스의 인스턴스 ds 생성
     ds = DummySensor()
     ds.set_env()
     print(ds.get_env())
 
+    # MissionComputer 클래스의 인스턴스 runComputer 생성
     runComputer = MissionComputer()
     runComputer.get_mission_computer_info()
     runComputer.get_mission_computer_load()
