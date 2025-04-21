246ce57b071eec44a23991fa024aa6668a0956ea
이재혁 <127727548+lyler-lee@users.noreply.github.com>
2025-04-21 19:07:19 +0900

    Update test.py

--- python/mission06/test.py
+++ python/mission06/test.py
@@ -1,9 +1,9 @@
 import sys 
 from PyQt5.QtWidgets import QApplication, QWidget
 
-app = QApplication(sys.argv)
+app = QApplication(sys.argv)  # QApplication  객체 생성
+  
+win = QWidget()              # window 생성
+win.show()                   # 창을 띄우는 역할
 
-win = QWidget()
-win.show()
-
-app.exec_()
+app.exec_()                  # 이벤트 루프 실행
